c31a18f4e04bc4faf71c16335569c95ef3f3b2d0
hikaru <hika1785040525@gmail.com>
2024-12-27 14:19:56 +0900

    fizzbuzz機能完成

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -2,7 +2,9 @@
 
 class FizzBuzz:
   def GetNumber(self, num:int) -> str:
-    if (num % 3) == 0:
+    if (num % 15) == 0:
+      StringNum = "FizzBuzz"
+    elif (num % 3) == 0:
       StringNum = "Fizz"
     elif (num % 5) == 0:
       StringNum = "Buzz"
--- src/test_fizzbuzz.py
+++ src/test_fizzbuzz.py
@@ -11,21 +11,32 @@
 #======Todo==================================================#
 # ・数字を文字列に変換する =======================> 完了
 # ・3の倍数の場合は"Fizz"に変換する===============> 完了
-# ・5の倍数の場合は"Buzz"に変換する
-# ・3と5の両方の倍数の場合は"FizzBuzz"をに変換する
+# ・5の倍数の場合は"Buzz"に変換する===============> 完了
+# ・3と5の両方の倍数の場合は"FizzBuzz"をに変換する=> 完了
 #===========================================================#
 
 import unittest 
 from FizzBuzz import FizzBuzz
 
 class FizzBuzzTest(unittest.TestCase):
+  # 数字を文字列に変換するテスト
   def test_NumToString(self):
     fizzbuzz = FizzBuzz()
+
+    # 3,5の倍数以外の場合
     self.assertEqual("1", fizzbuzz.GetNumber(1))
     self.assertEqual("2", fizzbuzz.GetNumber(2))
 
+    # 3を渡すとFizzを返す
     self.assertEqual("Fizz", fizzbuzz.GetNumber(3))
+    # 9を渡すとFizzを返すa
     self.assertEqual("Fizz", fizzbuzz.GetNumber(9))
 
+    # 5を渡すとBuzzを返す
     self.assertEqual("Buzz", fizzbuzz.GetNumber(5))
+    # 10を渡すとBuzzを返す
     self.assertEqual("Buzz", fizzbuzz.GetNumber(10))
+
+    # 15を渡すとFizzBuzzを返す
+    self.assertEqual("FizzBuzz", fizzbuzz.GetNumber(15))
+    self.assertEqual("FizzBuzz", fizzbuzz.GetNumber(60))
