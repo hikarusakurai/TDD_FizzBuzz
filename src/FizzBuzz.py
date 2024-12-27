bef330d6f1c99d51d9822be4d217b4cb47deb3a0
hikaru <hika1785040525@gmail.com>
2024-12-27 13:58:36 +0900

    リファクタリング

--- src/FizzBuzz.py
+++ src/FizzBuzz.py
@@ -2,9 +2,9 @@
 
 class FizzBuzz:
   def GetNumber(self, num:int) -> str:
-    if num == 3:
+    if (num % 3) == 0:
       StringNum = "Fizz"
     else:
       StringNum = str(num)
-      
+
     return StringNum
